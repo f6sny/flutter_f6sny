d46d42b4d7e1f3dc2063283fc7013b8169dba349
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-05-08 21:40:29 +0300

    adding fastlane

---
+++ ios/flutter_f6snyUITests/flutter_f6snyUITests.swift
@@ -0,0 +1,47 @@
+//
+//  flutter_f6snyUITests.swift
+//  flutter_f6snyUITests
+//
+//  Created by Mohannad Alotaibi on 08/05/2023.
+//
+
+import XCTest
+
+final class flutter_f6snyUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        let element = XCUIApplication().windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .other).element(boundBy: 1)
+        let scrollView = element.children(matching: .other).element(boundBy: 2).children(matching: .other).element(boundBy: 1).children(matching: .scrollView).element
+        scrollView.tap()
+        scrollView.swipeUp();
+                
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
